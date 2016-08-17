4158d8a4986b904d9dcf74ec55bbf6428032c1c1
bcecil88 <bcecil88@gmail.com>
2016-08-16 22:01:08 -0400

    private get
    edit for private access modifier

--- public class Example {.java
+++ public class Example {.java
@@ -1,9 +1,11 @@
 public class Example {
+    
     public static void main(String[] args) {
+        
         System.out.println("We are making a new Pez dispenser");
-		PezDispenser dispenser = new PezDispenser();
-		System.out.printf("The dispenser character is %s\n", 
-						  dispenser.mCharacterName);
+			PezDispenser dispenser = new PezDispenser();
+			System.out.printf("The dispenser character is %s\n", 
+							  dispenser.getCharacterName());
     }
 }
 //treehouse
