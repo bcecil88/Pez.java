c93d6a0638837e00b607af14586413f6a0d48523
bcecil88 <bcecil88@gmail.com>
2016-08-16 20:41:31 -0400

    oop
    top

---
+++ public class Example {.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("We are making a new Pez dispenser");
+		PezDispenser dispenser = new PezDispenser();
+		System.out.printf("The dispenser character is %s\n", 
+						  dispenser.mCharacterName);
+    }
+}
+//treehouse
